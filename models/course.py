05183e28e8549deb967318f4efd232defac11d67
Donatas Noreika <donoras@gmail.com>
2022-04-19 11:13:04 +0300

    odoo 3: added one2many field: sessions_ids

--- models/course.py
+++ models/course.py
@@ -10,4 +10,5 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', string="Responsible", ondelete='set null')
+    sessions_ids = fields.One2many('openacademy.session', 'course_id')
 

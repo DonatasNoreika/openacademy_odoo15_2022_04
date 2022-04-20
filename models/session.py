eb31766acf9efffbd61d2eb6371be65fd4ed7c15
Donatas Noreika <donoras@gmail.com>
2022-04-20 10:55:32 +0300

    odoo 4: added domain to session, instructor_id field

--- models/session.py
+++ models/session.py
@@ -8,7 +8,7 @@ class Session(models.Model):
     _description = "OpenAcademy Sessions"
 
     name = fields.Char(string="Name", required=True)
-    instructor_id = fields.Many2one('res.partner', string="Instructor", ondelete="set null")
+    instructor_id = fields.Many2one('res.partner', string="Instructor", ondelete="set null", domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', 'Teacher')])
     start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")

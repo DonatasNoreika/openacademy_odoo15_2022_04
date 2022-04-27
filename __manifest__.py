d7a764b14fef7ec4cce43d0b5739c13ea476439f
Donatas Noreika <donoras@gmail.com>
2022-04-27 10:07:32 +0300

    odoo 6: added access rights

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/security.xml',
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/course_view.xml',
         'views/session_view.xml',

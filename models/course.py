3aa36c77e88891d279d8c69ca0fc40f5c2514afc
Donatas Noreika <donoras@gmail.com>
2022-04-29 10:36:34 +0300

    odoo 8: added image and editable tree view in form

--- models/course.py
+++ models/course.py
@@ -11,6 +11,7 @@ class Course(models.Model):
     description = fields.Text(string=_("Description"))
     responsible_id = fields.Many2one('res.users', string="Responsible", ondelete='set null')
     sessions_ids = fields.One2many('openacademy.session', 'course_id')
+    image = fields.Image("Image", attachment=True)
 
     _sql_constraints = [
         ('name_description_check',
